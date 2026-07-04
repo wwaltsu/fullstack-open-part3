76345d910adeffd7ae54b09d6a6ba17c2695567a
waltsu <waltsumarcus@gmail.com>
2026-07-05 01:07:30 +0300

    feat: complete exercise 3.13

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const Person = require('./models/mongo')
 const express = require('express')
 const app = express()
 var morgan = require('morgan')
@@ -11,38 +12,10 @@ morgan.token('body', (req) => {
 
 app.use(morgan(':method :url :status :response-time ms :body'))
 
-let persons = [
-  {
-    name: 'Arto Hellas',
-    number: '040-123456',
-    id: '1'
-  },
-  {
-    name: 'Ada Lovelace',
-    number: '39-44-5323523',
-    id: '2'
-  },
-  {
-    name: 'Dan Abramov',
-    number: '12-43-234345',
-    id: '3'
-  },
-  {
-    name: 'Mary Poppendieck',
-    number: '39-23-6423122',
-    id: '4'
-  }
-]
-
-app.get('/', (request, response) => {
-  response.send('<h1>Hello world</h1>')
-})
-
-app.get('/info', (req, res) => {
-  res.send(`
-    <p>Phonebook has info for ${persons.length} people</p>
-    <p>${new Date()}</p>
-  `)
+app.get('/api/persons', (request, response) => {
+  Person.find({}).then((persons) => {
+    response.json(persons)
+  })
 })
 
 app.get('/api/persons/:id', (request, response) => {
@@ -100,8 +73,15 @@ app.post('/api/persons', (request, response) => {
   response.json(person)
 })
 
-app.get('/api/persons', (request, response) => {
-  response.json(persons)
+app.get('/', (request, response) => {
+  response.send('<h1>Hello world</h1>')
+})
+
+app.get('/info', (req, res) => {
+  res.send(`
+    <p>Phonebook has info for ${persons.length} people</p>
+    <p>${new Date()}</p>
+    `)
 })
 
 const PORT = process.env.PORT || 3001
